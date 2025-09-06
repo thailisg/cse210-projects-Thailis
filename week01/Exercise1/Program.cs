3bf088354d20ae5a1d79cbf2e874becbf2867b86
thailisg <thai.ori.gon.diaz@gmail.com>
2025-09-05 23:56:32 -0400

    I did the conditional sentences exercise.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,5 +11,44 @@ class Program
         string last_name = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last_name}, {name} {last_name}. ");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        float number = int.Parse(userInput);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        // message
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed. ");
+        }
+        else
+        {
+            Console.WriteLine("Nice try! good luck the next time. ");
+        }
     }
 }
