a34da3332b1372ee4a00f1b6ea32c729fd113b85
thailisg <thai.ori.gon.diaz@gmail.com>
2025-09-06 10:33:52 -0400

    I did exercise three which consisted of a number guessing game.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes" || keepPlaying == "Yes")
+        {
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            //Console.WriteLine($"{magicNumber}"); //in case of test
+
+            int guessNumber = 0;
+            int attempsCounter = 0;
+
+            while (guessNumber != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userInput2 = Console.ReadLine();
+                guessNumber = int.Parse(userInput2);
+
+                attempsCounter++;
+
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it! you tried {attempsCounter} times.");
+                }
+
+            }
+
+            Console.WriteLine($"Do you want to keep playing? (yes/no)");
+            keepPlaying = Console.ReadLine();
+
+        }
+
     }
 }
