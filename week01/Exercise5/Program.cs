5ea9aea1a45b7b312b90b1ba846acdd5bd21a42e
thailisg <thai.ori.gon.diaz@gmail.com>
2025-09-06 15:10:56 -0400

    I did exercise 5 which consisted of making functions to ask the user for their name, favorite number and that said number is displayed squared.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(name, squareNumber);
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string numberConsole = Console.ReadLine();
+            int numberUser = int.Parse(numberConsole);
+
+            return numberUser;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.Write($"{name}, the square of your number is: {square}");
+        }
+        
+    
 }
