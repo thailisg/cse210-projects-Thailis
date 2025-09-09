ef9e4387b677f6a2ffb6f4556b7fc469180f06d5
thailisg <thai.ori.gon.diaz@gmail.com>
2025-09-09 19:50:16 -0300

    I completed the "Abstraction" learning activity, which involved creating and implementing classes correctly

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2025;
+        job1._endYear = 2027;
+
+        Job job2 = new Job();
+
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software development";
+        job2._startYear = 2025;
+        job2._endYear = 2028;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Thailis Gonzalez";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
+
     }
 }
