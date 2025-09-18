f1ae694ccd1dfc0ddfe5cb4611050cf97412e87d
thailisg <thai.ori.gon.diaz@gmail.com>
2025-09-18 15:54:41 -0300

    learning activity W03: Encapsulation

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+using System.Dynamic;
+
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+    public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fractions(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"Fraction {_top} / {_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions fraction1 = new Fractions();
+        Fractions fraction2 = new Fractions(5);
+        Fractions fraction3 = new Fractions(3, 4);
+        Fractions fraction4 = new Fractions(1, 3);
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
     }
 }
