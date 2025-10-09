bcd432a2bf028bca0c0b1ad4a0135d293db6abf2
thailisg <thai.ori.gon.diaz@gmail.com>
2025-10-09 14:15:59 -0300

    Learning Activity W06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("blue", 4.5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("red", 5, 8);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("green", 6.5);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
